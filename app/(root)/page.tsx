b7cd1ac0dec1853269ac317840b56ac45100011d
Janm20 <jan.mackovic@datablast.eu>
2023-07-16 22:52:30 +0200

    setup completed

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <p>Hello admin dashboard</p>
+      <Button size="default" variant="destructive">
+        Click me
+      </Button>
+    </div>
+  );
+}
